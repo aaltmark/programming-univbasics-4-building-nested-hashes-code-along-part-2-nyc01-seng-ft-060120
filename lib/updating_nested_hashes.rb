b31662f7d42b03a5badc4d34c8240fa41a712ebe
Adam Altmark <adamaltmark@gmail.com>
2020-05-12 20:37:42 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -20,10 +20,9 @@ def update_status
       }
    }
 
-def update_status
-  epic_tragedy
   epic_tragedy[:montague][:hero][:status] = "dead"
   epic_tragedy[:capulet][:heroine][:status] = "dead"
+  epic_tragedy
 end
 
 def add_characters
